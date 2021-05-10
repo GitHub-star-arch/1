cf4ce43db5a02fa3df5076e85a5669970fbe778d
GitHub-star-arch <63858220+GitHub-star-arch@users.noreply.github.com>
2021-05-10 17:05:29 +0530

    Add files via upload

---
+++ Screens/MyBarters.js
@@ -0,0 +1,27 @@
+//All that imports exports everything like that
+import React from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
+import db from '../config'
+
+export default class Barters extends React.Component {
+
+  render() {
+    return (
+      <View>
+      </View>
+    );
+  }
+}
+
+
+const styles = StyleSheet.create({
+  input: {
+      backgroundColor: "coral",
+      borderWidth: 5,
+      width: 100,
+      height: 40,
+      marginTop: 10,
+      borderColor: "blue",
+  },
+})
